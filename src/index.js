2c876acafc48321cfc7e1d0ff763c8fba9db4d1c
Clover <sewsistacomo@gmail.com>
2026-05-18 20:08:20 -0500

    log square of numbers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-/* javascript
-    [Meredith] - [May 16, 2026]
-*/   
+javascript
+//Meredith Morrow 05/18/2026
 
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
     console.log(i);
+   console.log(i * i);
 }
