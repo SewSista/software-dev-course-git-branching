439cd191adfef3aeb49b6c3d232d1b04d7ac9798
Clover <sewsistacomo@gmail.com>
2026-05-18 20:12:02 -0500

    pdate author comment with purpose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-/* javascript
-    [Meredith] - [May 16, 2026]
-*/   
+    // Meredith Morrow - 05/18/2026
+   // This application prints "Hello, World!" and the numbers 0 to 9.
 
 console.log("Hello world!");
 
